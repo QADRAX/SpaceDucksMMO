ced5ac8022e95dc4ba7318dfdc1fd74339f8c19a
QADRAX <carlos.delacuadra@devo.com>
2026-01-31 14:03:19 +0100

    chore: remove usless packages

--- packages/core/src/domain/map/ids.ts
+++
@@ -1,6 +0,0 @@
-/** Identificador único para sistemas solares */
-export type SystemId = string;
-/** Identificador único para estrellas */
-export type StarId = string;
-/** Identificador único para planetas */
-export type PlanetId = string;
